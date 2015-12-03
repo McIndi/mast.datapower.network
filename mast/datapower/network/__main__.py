b55087c6c30269fdab359ab0efd550ff4fd23ddd
ilovetux <me@ilovetux.com>
2015-12-03 16:48:57 -0500

    Improved error handling

--- mast/datapower/network/__main__.py
+++ mast/datapower/network/__main__.py
@@ -4,5 +4,9 @@ from network import cli
 import sys
 sys.argv[0] = "mast-network"
 
-cli.Run()
-
+try:
+    cli.Run()
+except AttributeError, e:
+    if "'NoneType' object has no attribute 'app'" in e:
+        raise NotImplementedError(
+            "HTML formatted output is not supported on the CLI")
